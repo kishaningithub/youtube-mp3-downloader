2dca5f90b987bb91670b3d93423d11ea66fd8286
Kishan B <bkishan@thoughtworks.com>
2018-02-03 12:47:37 +0530

    Reduce the amount of variables

--- app.py
+++ app.py
@@ -9,13 +9,11 @@ output_folder="output"
 def download_mp3(youtube_url):
     yt = YouTube(youtube_url)
     stream = yt.streams.get_by_itag(140)
-    downloaded_m4a_file_name = stream.default_filename
-    downloaded_m4a_full_path = Path(downloaded_m4a_file_name)
-    converted_mp3_file_name = f"{os.path.splitext(downloaded_m4a_file_name)[0]}.mp3"
-    converted_mp3_full_path = Path(output_folder).joinpath(converted_mp3_file_name)
+    downloaded_m4a_full_path = Path(stream.default_filename)
+    converted_mp3_full_path = Path(output_folder).joinpath(f"{downloaded_m4a_full_path.stem}.mp3")
 
     if converted_mp3_full_path.exists():
-        print(f"'{converted_mp3_file_name}' already exists. Skipping...")
+        print(f"'{converted_mp3_full_path.name}' already exists. Skipping...")
     else: 
         print(f"Downloading '{yt.title}'")
         Path(output_folder).mkdir(exist_ok=True)
